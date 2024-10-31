9d35964177153431304aea5dbf46a9be8b4c442f
DanielStanecki <daniel.stanecki@laetottenham.ac.uk>
2024-10-31 16:59:35 +0000

    work                                                                 sdaf

--- main.py
+++ main.py
@@ -1,32 +1,38 @@
 cat_attributes = {
-    "intelligence": 0,
-    "energy": 0,
-    "weight": 0,
+    "intelligence": 10,
+    "energy": 50,
+    "weight": 30,
     # change the inital values above
 }
 
 print("Welcome to my cat game!!")
 
 # Take the user inputs for name and colour:
-name = input("Enter name:")
-# ...
-
+name = input("Please enter the name of your cat: ")
+colour = input("Please enter the colour of the cat: ")
+x = True
 # start the while loop
-while True:
+while x == True:
     # Finish the string below
-    option = input("What would you like to do? 1. Play with your cat 2. Train your cat 3. show stats")
-
+    option = input("What would you like to do? 1. Play with your cat 2. Train your cat 3. show stats ")
     if option == '1':
-        # change the cat's attributes here
+        print("You are playing with your cat!!")
+        cat_attributes["energy"] -= 5
         pass
     elif option == '2':
-        pass
-    # elif ...
+        print("You are training your cat!!")
+        cat_attributes["energy"] -= 10
+        cat_attributes["weight"] -= 5
+        cat_attributes["intelligence"] += 5
+    elif option == "3": 
+        print(cat_attributes)
     else:
         pass
 
     # finish off the if statements below
-    if cat_attributes['energy'] < 0:
+    if cat_attributes['energy'] < 0 or cat_attributes["weight"] < 0:
+        print("The cat is dead due to your actions.")
+        x = False 
         pass
     # elif ...
     
