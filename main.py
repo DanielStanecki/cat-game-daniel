90725d1294511eb5d0fcf4027d58270b99307343
DanielStanecki <daniel.stanecki@laetottenham.ac.uk>
2024-10-31 17:00:27 +0000

    catgame

--- main.py
+++ main.py
@@ -1,38 +1,57 @@
 cat_attributes = {
     "intelligence": 10,
     "energy": 50,
-    "weight": 30,
-    # change the inital values above
+    "weight": 10,
 }
 
 print("Welcome to my cat game!!")
 
-# Take the user inputs for name and colour:
-name = input("Please enter the name of your cat: ")
+name = input("Please enter the name of your cat: ").title()
 colour = input("Please enter the colour of the cat: ")
 x = True
-# start the while loop
+
 while x == True:
-    # Finish the string below
-    option = input("What would you like to do? 1. Play with your cat 2. Train your cat 3. show stats ")
+    
+    option = input("What would you like to do? 1. Play with your cat 2. Train your cat 3. Feed your cat 4. Put your cat to sleep 5. Show cat stats ")
     if option == '1':
-        print("You are playing with your cat!!")
-        cat_attributes["energy"] -= 5
-        pass
+        if cat_attributes["energy"] <= 5:
+            print(f"{name} is too tired")
+        else:
+            print(f"You are playing with {name}!!")
+            cat_attributes["energy"] -= 5
     elif option == '2':
-        print("You are training your cat!!")
-        cat_attributes["energy"] -= 10
-        cat_attributes["weight"] -= 5
-        cat_attributes["intelligence"] += 5
+        if cat_attributes["energy"] <= 5:
+            print(f"{name} is too tired")
+        else: 
+            print(f"You are training {name}!!")
+            cat_attributes["energy"] -= 10
+            cat_attributes["weight"] -= 5
+            cat_attributes["intelligence"] += 5
     elif option == "3": 
+        if cat_attributes["weight"] >= 30 and cat_attributes["energy"] >= 100:
+            print(f"{name} is too energetic AND needs to eat a bit less!!")
+        elif cat_attributes["energy"] >= 100:
+            print(f"{name} is already too energetic!!")
+        elif cat_attributes["weight"] >= 30: 
+            print(f"{name} needs to eat less!!")
+        else:
+            print(f"You are feeding {name}!!")
+            cat_attributes["energy"] += 5
+            cat_attributes["weight"] += 5
+    elif option == "4": 
+        if cat_attributes["energy"] >= 100: 
+            print(f"{name} is too energetic too sleep right now!!")
+        else:
+            print(f"{name} is sleeping.")
+            cat_attributes["energy"] += 10
+    elif option == "5": 
         print(cat_attributes)
     else:
         pass
 
-    # finish off the if statements below
-    if cat_attributes['energy'] < 0 or cat_attributes["weight"] < 0:
-        print("The cat is dead due to your actions.")
+    if cat_attributes['energy'] <= 0 or cat_attributes["weight"] <= 0:
+        print(f"{name} is dead due to your actions.")
         x = False 
-        pass
-    # elif ...
+        
+
     
